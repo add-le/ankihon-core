49339ce323b7c2103f03b175fe5ba4b0760e6586
add-le <add.lelann@pm.me>
2024-10-27 22:58:58 +0100

    feat(manifest): set correct start_url

--- app/manifest.ts
+++ app/manifest.ts
@@ -20,7 +20,7 @@ export default function manifest(): MetadataRoute.Manifest {
       },
     ],
     id: "/?source=pwa",
-    start_url: `${basePath}/toto/tata`,
+    start_url: `${basePath}/main/learn`,
     background_color: "#E3DED2",
     display: "standalone",
     scope: "/",
