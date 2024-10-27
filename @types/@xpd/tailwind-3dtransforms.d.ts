4cdff731d3ac5199fc0b8e1b66e1a24456477029
add-le <add.lelann@pm.me>
2024-10-27 16:17:21 +0100

    feat(build): build static app

---
+++ @types/@xpd/tailwind-3dtransforms.d.ts
@@ -0,0 +1,5 @@
+declare module "@xpd/tailwind-3dtransforms" {
+  import type { Plugin } from "tailwindcss";
+  const tailwind3dTransformPlugin: Plugin;
+  export default tailwind3dTransformPlugin;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import Init from "./init";
 
