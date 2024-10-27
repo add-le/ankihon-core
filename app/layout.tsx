1f5d7c0992ecedd49a35f7c3af36281c77f0acbc
add-le <add.lelann@pm.me>
2024-10-27 20:30:23 +0100

    feat(manifest): hard coded path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Init from "./init";
-const {
-  default: { basePath },
-} = await import("../next.config.mjs");
 
 export const metadata: Metadata = {
-  manifest: `${basePath}/manifest.webmanifest`,
+  manifest: `/ankihon-core/manifest.webmanifest`,
 };
 
 export default function RootLayout({
