e6773d70b31c95589cc1887067ce3d4ae70a23b8
add-le <add.lelann@pm.me>
2024-10-28 21:17:01 +0100

    feat(gitignore): remove build

--- scripts/gh-pages.ts
+++ scripts/gh-pages.ts
@@ -4,15 +4,17 @@ import ghpages from "gh-pages";
 const EXIT_SUCCESS = 0;
 const EXIT_FAILURE = 1;
 
-try {
-  ghpages.publish("out", {
+ghpages
+  .publish("out", {
     message: "Deployed to GitHub Pages",
     nojekyll: true,
+    dotfiles: false,
+  })
+  .then(() => {
+    console.log(` ${chalk.green("✓")} Successfully deployed to GitHub Pages`);
+    process.exit(EXIT_SUCCESS);
+  })
+  .catch((error) => {
+    console.error(` ${chalk.red("✗")} Error deploying to GitHub Pages:`, error);
+    process.exit(EXIT_FAILURE);
   });
-
-  console.log(`${chalk.green("✓")} Successfully deployed to GitHub Pages`);
-  process.exit(EXIT_SUCCESS);
-} catch (error) {
-  console.error(`${chalk.red("✗")} Error deploying to GitHub Pages:`, error);
-  process.exit(EXIT_FAILURE);
-}
